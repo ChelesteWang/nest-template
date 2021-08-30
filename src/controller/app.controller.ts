13f8b560f17661baa91895f2b85abd0352f13c65
CelesteWang <1581446178@qq.com>
2021-08-30 19:34:52 +0800

    fix: format code

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from '../service/app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/hello")
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
   }
